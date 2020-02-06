ed42e31c2d09c6cb55ded30d2429b188eaa5fa30
Ismael Abreu <ismaelga@gmail.com>
2020-02-06 16:41:05 +0000

    Improve css loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
-import 'normalize-css'
+// import 'normalize-css'
+
+import * as React from 'react'
+import { cache } from 'emotion'
+import { CacheProvider } from '@emotion/core'
+
+export const wrapRootElement = ({ element }) => {
+  return <CacheProvider value={cache}>{element}</CacheProvider>
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,12 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+            integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+            crossorigin="anonymous"
+          />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
@@ -37,7 +43,8 @@ const Layout = ({ children }) => (
             maxWidth: 700,
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
-            fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif'
+            fontFamily:
+              '-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif',
           }}
         >
           {children}
