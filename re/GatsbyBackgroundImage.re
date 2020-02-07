a4a63bdd64407f409d7b077568c70d8da1f127f1
Filipe Sousa <fpsousa91@gmail.com>
2020-02-07 23:47:42 +0000

    wip: team detail

---
+++ re/GatsbyBackgroundImage.re
@@ -0,0 +1,2 @@
+[@react.component] [@bs.module "gatsby-background-image"]
+external make: (~fluid: unit, ~className: string, ~children: React.element) => React.element = "default";
---
+++ src/pages/teamDetail.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import TeamDetail from 're/TeamDetail'
+import Layout from 'src/components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const developer = {
+  name: "Diogo Nunes",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi u",
+  headline: "has a vast experience and deep knowledge on all things on iOS",
+  photo: "images/team/CarlosMatias.png"
+};
+
+
+
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+  developerImage: file(relativePath: { eq: "team/CarlosMatias.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 1200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`)
+
+  return (
+    <Layout useDarkNavBarLinks>
+      <TeamDetail
+        developer={developer}
+        image={data.developerImage.childImageSharp.fluid}
+      />
+    </Layout>
+  )
+}
+
+export default IndexPage
