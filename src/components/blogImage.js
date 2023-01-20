2c93ccd3b85255eb0728bdba4b7cbb71145a4dce
Joao Barbosa <joao.barbosa@glazedsolutions.com>
2023-01-20 16:17:53 +0000

    Re-adding blogImage

--- src/components/blogImage.js
+++ src/components/blogImage.js
@@ -1,4 +1,3 @@
-/*
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
@@ -59,4 +58,3 @@ export default function Image({ src, alt, caption }) {
     </figure>
   )
 }
-*/
